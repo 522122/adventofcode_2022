84e3681e7efb138e22918cce417d17a1fdea7f0a
522122 <lolofon@icloud.com>
2022-12-06 07:14:38 +0100

    refactor

--- src/6/index.ts
+++ src/6/index.ts
@@ -1,60 +1,21 @@
 import fs from 'fs/promises'
 import path from 'path'
 
+const createChecker = (str: string) => (size: number) => {
+  for (let i = 0; i < str.length; i++) {
+    if ([...new Set(str.substring(i, i + size))].length === size) {
+      return i + size
+    }
+  }
+}
+
 const main = async () => {
   const buffer = await fs.readFile(path.join(__dirname, 'data'))
 
-  const data = buffer.toString()
+  const check = createChecker(buffer.toString())
 
-  let substr = ''
-  let substr2 = ''
-  let pt1Done = false
-  let pt2Done = false
-
-  for (let i = 0; i < data.length; i++) {
-    if (data[i] == null || (pt1Done && pt2Done)) break
-
-    if (!pt1Done) {
-      substr += data[i]
-
-      if (substr.length > 4) {
-        substr = substr.substring(1)
-      }
-
-      if (substr.length === 4 && [...new Set(substr)].length === 4) {
-        console.log(`pt1: ${i + 1}`)
-        pt1Done = true
-      }
-    }
-
-    if (!pt2Done) {
-      substr2 += data[i]
-
-      if (substr2.length > 14) {
-        substr2 = substr2.substring(1)
-      }
-
-      if (substr2.length === 14 && [...new Set(substr2)].length === 14) {
-        console.log(`pt2: ${i + 1}`)
-        pt2Done = true
-      }
-    }
-  }
-
-  const test = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
-  let subTest = ''
-  for (let i = 0; i < test.length; i++) {
-    subTest += test[i]
-
-    if (subTest.length > 14) {
-      subTest = subTest.substring(1)
-    }
-
-    if (subTest.length === 14 && [...new Set(subTest)].length === 14) {
-      console.log(`pt2 test: ${i + 1}`)
-      break
-    }
-  }
+  console.log(check(4))
+  console.log(check(14))
 }
 
 export default main
