c7088c19119c8d6f74ba69635e02021c78718365
522122 <lolofon@icloud.com>
2022-12-13 23:48:40 +0100

    utils

---
+++ utils.ts
@@ -0,0 +1,17 @@
+import fs from 'fs'
+import path from 'path'
+
+export const readInput = (name: string = 'data') => {
+  return fs
+    .readFileSync(path.join(__dirname, 'src', process.argv[2], name))
+    .toString()
+}
+
+type TraverseCallback<T> = (value: T, i: number, j: number) => void
+export const traverse = <T>(arr: T[][], callback: TraverseCallback<T>) => {
+  for (let i in arr) {
+    for (let j in arr[i]) {
+      callback(arr[i][j], Number(i), Number(j))
+    }
+  }
+}
